215f885bfb1a41ab3d6b8feb3ebe5ee278682840
xu-hong <xuhong713@gmail.com>
2015-09-24 17:40:17 -0400

    first commit

---
+++ hw2/toyline_steorts.R
@@ -0,0 +1,11 @@
+# Steorts, R., 2015-08-16, Homework 1
+a <- 2
+b <- 7
+sigSq <- 0.5
+n <- 400
+set.seed(1234)
+x <- runif(n)
+y <- a + b * x + rnorm(n, sd = sqrt(sigSq))
+(avgX <- mean(x))
+plot(x, y)
+abline(a, b, col = "blue", lwd = 2)
